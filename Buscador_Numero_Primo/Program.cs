a19d5d507ec935bede7fb8ee8a377b15eb4000ed
Maria Lugo <mariafernanda.lugobolivar@gmail.com>
2024-06-19 17:41:08 -0400

    Finalizado

---
+++ Buscador_Numero_Primo/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Buscador de Números Primos.");
+bool EsPrimo(int numero)
+{
+    if (numero < 2)
+        return false;
+
+    for (int i = 2; i <= Math.Sqrt(numero); i++)
+    {
+        if (numero % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+Console.WriteLine("Por favor ingresa el número inicial del rango:");
+int inicio = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Por favor ingresa el número final del rango:");
+int fin = int.Parse(Console.ReadLine());
+
+List<int> PrimosEncontrados = new List<int>();
+
+for (int num = inicio; num <= fin; num++)
+{
+    if (EsPrimo(num))
+        PrimosEncontrados.Add(num);
+}
+
+Console.WriteLine("Números primos encontrados:");
+foreach (int primo in PrimosEncontrados)
+{
+    Console.WriteLine(primo + " ");
+}
+
